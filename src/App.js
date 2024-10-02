2c36efbd130186bd547a2684055d8793e1542597
jidibk <101001264+jidibk@users.noreply.github.com>
2024-10-02 14:08:44 +0100

    made a calculator app

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 // import logo from './logo.svg';
 import './App.css';
 // import Quote from './Quote'
-import Calculator from './calculator';
+import Calculator from './components/calculator';
 
 
 
